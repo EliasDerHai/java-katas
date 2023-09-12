76611ef96884b43c13c2eb61514eca376d1d90ed
Elias Haider <e.haider@intive.com>
2023-09-12 11:48:54 +0200

    add solution for RomanNumeralDecoder

--- src/romannumeral/RomanNumeralDecoder.java
+++ src/romannumeral/RomanNumeralDecoder.java
@@ -3,7 +3,45 @@ package romannumeral;
 public class RomanNumeralDecoder {
 
     static int decodeRoman(String romanNumbers) {
-        // TODO
-        return -1;
+        char[] romanChars = romanNumbers.toUpperCase().toCharArray();
+
+        int lastCharVal = 0;
+        int total = 0;
+
+        for (char c : romanChars) {
+            int currentValue = getNumeralValue(c);
+
+            if (currentValue > lastCharVal) {
+                total -= lastCharVal;
+            } else {
+                total += lastCharVal;
+            }
+
+            lastCharVal = currentValue;
+        }
+        total += lastCharVal;
+
+        return total;
+    }
+
+    private static int getNumeralValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1_000;
+            default:
+                throw new IllegalArgumentException("Invalid symbol");
+        }
     }
 }
--- src/romannumeral/RomanNumeralDecoderTest.java
+++ src/romannumeral/RomanNumeralDecoderTest.java
@@ -8,11 +8,16 @@ import static romannumeral.RomanNumeralDecoder.decodeRoman;
 class RomanNumeralDecoderTest {
 
     @Test
-    void shouldSort_snailSorted() {
+    void shouldWork() {
         assertEquals(21, decodeRoman("XXI"));
         assertEquals(1, decodeRoman("I"));
         assertEquals(4, decodeRoman("IV"));
         assertEquals(2008, decodeRoman("MMVIII"));
         assertEquals(1666, decodeRoman("MDCLXVI"));
+    }
+
+    @Test
+    void shouldDoEmpty() {
+        assertEquals(0, decodeRoman(""));
     }
 }
